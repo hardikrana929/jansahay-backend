81d7eeef96415db42f299b49dccd877978466e47
Asavala Hardik <hardikrana929@gmail.com>
2026-07-31 19:27:34 +0530

    change cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -81,11 +81,11 @@ const loginUser = async (req, res) => {
         }
 
         const token = generateToken(user._id);
-
+        const isProd = process.env.NODE_ENV === "production";
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: isProd,                 
+            sameSite: isProd ? "none" : "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
